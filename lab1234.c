c13db0c9bc3008faa5c85374d3cf2fc7266e8e23
lazysweet <altinbekaya2005@gmail.com>
2024-01-26 17:39:38 +0300

    lab1 not work

---
+++ lab1234.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+int sort(int n);
+
+int main(){
+     printf("Введите число\n");
+     int n;
+      if(scanf("%d", &n)!=1 || n<0 || (long int)n!=n}{
+       printf("Мы так не умеем, пока");
+       return -1;
+      }
+     printf("%d",sort (n));
+     return 0;
+}
+
+int sort(int n){
+    int itog=0;
+    int j;
+    int d=n;
+    for (int i=1;i<=9;i++){
+      j=n;
+      while(j!=0){
+       if(j%10==i){
+	itog*=10;
+        itog+=j%10;
+       }
+       if(itog>0 && itog<10){
+        while(d!=0){
+          if(d%10==0){
+           itog*=10;
+          }
+	  d/=10;
+        }
+      }
+    j/=10;
+      }
+    }
+return itog;
+}
---
+++ makefile
@@ -0,0 +1,2 @@
+main:
+	cc lab1234.c -o prog
